e3fd68ebc4510e4fdce8edcf420eeab0f1c1a311
mcbsd1 <59873315+mcbsd1@users.noreply.github.com>
2025-02-20 22:41:33 +0000

    Update run_nextflow.sh

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
 
+#SBATCH -p compute_amd
+#SBATCH --mem=80GB
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --tasks-per-node=1
+#SBATCH -t 08:00:00 
+#SBATCH -o slurm/%J.out
+#SBATCH -e slurm/%J.err
+#SBATCH --job-name=NEXT
+#SBATCH --account=scw2358
+
+myDir=$(pwd)
+
+module load java 
+module load nextflow/24.10.4
+module load cellranger/7.2.0
+
+nextflow run ${myDir}/main.nf --aggr true -with-trace
+
+# to run
+# sbatch run_nextflow.sh

b4ab35a6236efe63b5863626d98a5f12eb5076a1
mcbsd1 <59873315+mcbsd1@users.noreply.github.com>
2025-02-20 22:45:43 +0000

    Update download-ref-genome.sh

--- bin/download-ref-genome.sh
+++ bin/download-ref-genome.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+wget https://ftp.ensembl.org/pub/release-113/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna_sm.primary_assembly.fa.gz
+wget https://ftp.ensembl.org/pub/release-113/gtf/mus_musculus/Mus_musculus.GRCm39.113.gtf.gz
+gunzip *.gz
